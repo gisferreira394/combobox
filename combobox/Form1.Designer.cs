13917e06e497eaf25f35e81a120e529cac607d74
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-04-14 20:13:11 -0300

    .

--- combobox/Form1.Designer.cs
+++ combobox/Form1.Designer.cs
@@ -32,10 +32,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // comboBox
             // 
+            this.comboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.comboBox.FormattingEnabled = true;
             this.comboBox.Items.AddRange(new object[] {
             "1",
@@ -50,7 +54,7 @@
             "10",
             "11",
             "12"});
-            this.comboBox.Location = new System.Drawing.Point(214, 51);
+            this.comboBox.Location = new System.Drawing.Point(277, 108);
             this.comboBox.Name = "comboBox";
             this.comboBox.Size = new System.Drawing.Size(144, 21);
             this.comboBox.TabIndex = 0;
@@ -58,40 +62,88 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 54);
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 109);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.Size = new System.Drawing.Size(250, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Escolha um número de 1 a 12 : ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(389, 54);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(525, 99);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.Size = new System.Drawing.Size(180, 35);
             this.button1.TabIndex = 2;
             this.button1.Text = "Escolha um número e clique aqui";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(701, 401);
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(701, 392);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 32);
             this.button2.TabIndex = 3;
             this.button2.Text = "Sair";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(39, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Calligraphy", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Digite seu nome: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.textBox1.Location = new System.Drawing.Point(217, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.Opacity = 0.96D;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -104,6 +156,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- combobox/Form1.cs
+++ combobox/Form1.cs
@@ -16,5 +16,59 @@ namespace combobox
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome = textBox1.Text;
+
+            switch (Convert.ToInt32(comboBox.SelectedItem))
+            {
+                case 1:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em janeiro"; break;
+                case 2:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Fevereiro"; break;
+
+                case 3:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Março"; break;
+                case 4:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Abril"; break;
+
+                case 5:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Maio"; break;
+
+                case 6:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Junho"; break; ;
+
+                    case 7:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Julho"; break;
+
+                case 8:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Agosto"; break;
+
+                case 9:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Setembro"; break;
+
+                case 10:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Outubro"; break;
+
+                case 11:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Novembro"; break;
+
+                case 12:
+                    label2.Text = "Olá " + textBox1.Text + "Você nasceu em Dezembro"; break;
+
+                default:
+                    label2.Text = "Por favor um número; mês não encontrado";break;
+
+
+
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
